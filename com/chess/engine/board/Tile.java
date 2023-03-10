9a7462f8af60c92dee646e613fb89a5b4c4a4fdd
Oscar Garcia <oscarg327@gmail.com>
2023-03-09 18:52:46 -0600

    Knight, Move

--- com/chess/engine/board/Tile.java
+++ com/chess/engine/board/Tile.java
@@ -15,7 +15,7 @@ public abstract class Tile {
 
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
 
-        for(int i = 0; i < 64; i++) {
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++) {
             emptyTileMap.put(i, new EmptyTile(i));
         }
 
@@ -27,7 +27,7 @@ public abstract class Tile {
     }
     
     // Constructor 
-   private Tile(int tileCoordinate){
+   private Tile(final int tileCoordinate){
         this.tileCoordinate = tileCoordinate;
     }
 
@@ -57,7 +57,7 @@ public abstract class Tile {
         private final Piece pieceOnTile;
 
         // Constructor
-        private OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+        private OccupiedTile(int tileCoordinate, final Piece pieceOnTile) {
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
--- com/chess/engine/pieces/Piece.java
+++ com/chess/engine/pieces/Piece.java
@@ -1,6 +1,6 @@
 package com.chess.engine.pieces;
 
-import java.util.List;
+import java.util.Collection;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
@@ -17,5 +17,9 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
     }
 
-    public abstract List<Move> calculateLegalMoves(final Board board);
+    public Alliance getPieceAlliance() {
+        return this.pieceAlliance;
+    }
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 }
