ab82924f38450af4e050dff59cf7c9e0c80381b9
Oscar Garcia <oscarg327@gmail.com>
2023-03-13 18:54:23 -0500

    Pawn

--- com/chess/engine/pieces/Piece.java
+++ com/chess/engine/pieces/Piece.java
@@ -10,15 +10,21 @@ public abstract class Piece {
     
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
+    protected final boolean isfirstMove;
 
     Piece( final int piecePosition, final Alliance pieceAlliance){
 
         this.piecePosition = piecePosition;
         this.pieceAlliance = pieceAlliance;
+        this.isfirstMove = false;
     }
 
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
+    }
+
+    public boolean isfirstMove(){
+        return this.isfirstMove;
     }
 
     public abstract Collection<Move> calculateLegalMoves(final Board board);
